a5231cf8ab508481ea3aa24cfd951322e21e5889
MathXCruz <matheus.cruz1996@hotmail.com>
2022-05-16 21:31:33 -0300

    implemented sentry

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from uncoupledetl.transform import WoWTransformer, PokeTransformer
 from uncoupledetl.load import Loader, load_to_local_database
 from uncoupledetl.variables import URL, ENDPOINTS, WOW_URL, WOW_ENDPOINT
 import asyncio
+import logging
+import traceback
+from sentry_sdk.integrations.logging import LoggingIntegration
+import sentry_sdk
+import os
 
 
 async def main():
@@ -17,7 +22,29 @@ async def main():
     transformed_wow = WoWTransformer(wow_data).transform()
     transformed_poke = PokeTransformer(poke_data).transform()
     await Loader(transformed_poke).load_strategy(load_to_local_database)
+    logging.info('Test run completed.')
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    logging.basicConfig(
+        level=logging.INFO,
+        filename='logs.log',
+        format='%(name)s - %(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        filemode='a',
+    )
+
+    sentry_logging = LoggingIntegration(
+        level=logging.INFO, event_level=logging.INFO
+    )
+
+    sentry_sdk.init(
+        dsn=os.environ.get('sentry_test'), integrations=[sentry_logging]
+    )
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        tb = ''.join(traceback.format_tb(e.__traceback__))
+        tb = tb.replace('\n', ' - ')
+        logging.critical(f'main(uncoupledetl): unexpected error - {e} {tb}')
+        raise e
